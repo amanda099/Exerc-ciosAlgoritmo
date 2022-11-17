2dca21e61955db42dfafd2da879c956602726b8e
amanda <85532804+amanda099@users.noreply.github.com>
2022-11-17 11:03:20 -0400

    Add files via upload

---
+++ Ex_4.1/Program.cs
@@ -0,0 +1,16 @@
+﻿//1.Leia 4 valores, calcule a soma entre eles e apresente o resultado.
+
+float a, b, c, d, soma;
+
+Console.WriteLine("Digite 4 números");
+a = float.Parse(Console.ReadLine());
+b = float.Parse(Console.ReadLine());
+c = float.Parse(Console.ReadLine());
+d = float.Parse(Console.ReadLine());
+
+soma = a + b + c + d;
+
+Console.WriteLine("A soma entre os números digitados é:");
+Console.WriteLine(soma);
+
+Console.ReadKey();
